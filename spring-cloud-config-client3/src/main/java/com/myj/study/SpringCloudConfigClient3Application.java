04447f90625ff3652251b7d4630b17710b0c495d
myjgithubdl <1184010176@qq.com>
2017-10-31 14:56:42 +0800

    Spring Cloud Config RabbitMQ实现重新读取配置文件

---
+++ spring-cloud-config-client3/src/main/java/com/myj/study/SpringCloudConfigClient3Application.java
@@ -0,0 +1,12 @@
+package com.myj.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClient3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClient3Application.class, args);
+	}
+}
