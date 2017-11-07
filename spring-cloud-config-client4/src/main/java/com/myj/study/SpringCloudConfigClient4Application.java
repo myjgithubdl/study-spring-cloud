c30312606145aa46f9ad250f9ac0e9c1852897e0
myjgithubdl <1184010176@qq.com>
2017-11-07 09:20:24 +0800

    Spring Cloud Bus Kafka实现重新读取配置文件

---
+++ spring-cloud-config-client4/src/main/java/com/myj/study/SpringCloudConfigClient4Application.java
@@ -0,0 +1,12 @@
+package com.myj.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClient4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClient4Application.class, args);
+	}
+}
