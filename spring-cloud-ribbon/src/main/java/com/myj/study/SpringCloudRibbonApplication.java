4cb228e9f724ce2e5007bfaff153c25844170c34
myjgithubdl <1184010176@qq.com>
2017-11-10 09:31:44 +0800

    增加如在均衡策略

--- spring-cloud-ribbon/src/main/java/com/myj/study/SpringCloudRibbonApplication.java
+++ spring-cloud-ribbon/src/main/java/com/myj/study/SpringCloudRibbonApplication.java
@@ -1,5 +1,7 @@
 package com.myj.study;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -19,6 +21,13 @@ public class SpringCloudRibbonApplication {
 	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	//负载均衡策略
+	@Bean
+	public IRule ribbonRule() {
+		return new RandomRule();//随机策略
+		//return new BestAvailableRule();//并发量
 	}
 
 }
